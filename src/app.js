2e4dc06d7963bdda3c9bf7ceb1abcfe0eb79b4b4
prithul-paul <ptithulpaul@gmail.com>
2025-05-07 01:20:36 +0530

    Exploring 'Get', 'Post', 'Delete' HTTP request

--- src/app.js
+++ src/app.js
@@ -2,17 +2,28 @@ const express = require("../node_modules/express");
 
 const app = express();
 
-app.use("/", (req, res)=>{
-    res.send("Hello From Dashboard!!!!!");
+
+
+app.get("/user", (req, res)=>{
+    res.send({firstName: "Prithul", lastName:"Paul"})
+})
+app.post("/user", (req, res)=>{
+    res.send("Data saved successfully");
+})
+app.delete("/user", (req, res)=>{
+    res.send("Data deleted successfully");
+})
+app.use("/user", (req, res)=>{
+    res.send("User for all");
 })
 
-app.use("/hello", (req, res)=>{
-    res.send("Hello!!!!");
-})
-
-app.use("/about", (req, res)=>{
+app.use("/test", (req, res)=>{
     res.send("Hello World!");
 })
+
+
+
+
 
 app.listen(3000, ()=>{
     console.log("Server is running....");
