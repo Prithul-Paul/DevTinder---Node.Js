524b04e09b31998b8d43c75e6534b0657d85bae5
prithul-paul <ptithulpaul@gmail.com>
2025-05-25 21:43:37 +0530

    Feed API

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,5 @@
 const ConnectionRequest = require("../models/connection-request");
+const User = require("../models/users");
 
 const connectionRequests = async (req, res) => {
     try{
@@ -52,7 +53,35 @@ const connectedUsers = async (req, res) => {
     }
 }
 
+const userFeed = async (req, res) => {
+    try{
+        const loggedInUserId = req.user._id;
+        const hideUsersOnFeed = await ConnectionRequest.find({
+            $or: [
+                {formUserId: loggedInUserId},
+                {toUserId: loggedInUserId}
+            ]
+        }).select("formUserId toUserId");
+
+        const hideUserIds = new Set();
+
+        hideUsersOnFeed.forEach((row)=>{
+            hideUserIds.add(row.formUserId.toString());
+            hideUserIds.add(row.toUserId.toString());
+        });
+        // console.log(hideUserIds);
+
+        const usersOnfeed = await User.find({
+            _id : {$nin: Array.from(hideUserIds)}
+        }).select("firstName lastName age gender photoURL about skills");
+
+        res.json({data : usersOnfeed});
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+}
 module.exports = {
     connectionRequests,
-    connectedUsers
+    connectedUsers,
+    userFeed
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,7 @@ const userController = require("../controller/userController");
 
 router.get("/user/requests", middelwares.userAuth, userController.connectionRequests);
 router.get("/user/connection", middelwares.userAuth, userController.connectedUsers);
+router.get("/user/feed", middelwares.userAuth, userController.userFeed);
 
 
 
