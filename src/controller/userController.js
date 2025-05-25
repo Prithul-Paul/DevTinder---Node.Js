c338699dbfd8bce60a54b16807353bbc20424150
prithul-paul <ptithulpaul@gmail.com>
2025-05-26 01:23:49 +0530

    Feed API enhancement with Age and Common Skills

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const ConnectionRequest = require("../models/connection-request");
 const User = require("../models/users");
 
@@ -56,6 +57,12 @@ const connectedUsers = async (req, res) => {
 const userFeed = async (req, res) => {
     try{
         const loggedInUserId = req.user._id;
+        const loggedInUserAge = req.user.age;
+        const loggedInUserSkills = req.user.skills;
+
+        const maxAgeRange = loggedInUserAge + 5;
+        const minAgeRange = loggedInUserAge - 5;
+
         const hideUsersOnFeed = await ConnectionRequest.find({
             $or: [
                 {formUserId: loggedInUserId},
@@ -69,13 +76,70 @@ const userFeed = async (req, res) => {
             hideUserIds.add(row.formUserId.toString());
             hideUserIds.add(row.toUserId.toString());
         });
-        // console.log(hideUserIds);
+        console.log(hideUserIds);
+        const excludedIds = Array.from(hideUserIds)
+        const excludedObjectIds = excludedIds.map(id => new mongoose.Types.ObjectId(id));
+        // const priorityUsersOnfeed = await User.find({
+        //     _id : {$nin: Array.from(hideUserIds)},
+        //     age : {$gte : minAgeRange , $lte : maxAgeRange}
+        // }).select("firstName lastName age gender photoURL about skills");
 
-        const usersOnfeed = await User.find({
-            _id : {$nin: Array.from(hideUserIds)}
-        }).select("firstName lastName age gender photoURL about skills");
+        // const normalUsersOnfeed = await User.find({
+        //     _id : {$nin: Array.from(hideUserIds)},
+        //     $or : [
+        //         {age : {$gte : maxAgeRange , $lte : minAgeRange}},
+        //         {age : {$exists : false}},
+        //         {age :  null}
+        //     ]
+        // }).select("firstName lastName age gender photoURL about skills");
 
-        res.json({data : usersOnfeed});
+
+        const users = await User.aggregate([
+                {
+                    $match: {
+                    _id: { $nin: excludedObjectIds }
+                    }
+                },
+                {
+                    $addFields: {
+                        isCloseInAge: {
+                            $cond: [
+                                {
+                                    $and: [
+                                    { $gte: ["$age", minAgeRange] },
+                                    { $lte: ["$age", maxAgeRange] }
+                                    ]
+                                },1,0
+                            ]
+                        },
+                        commonSkillsCount: {
+                            $size: {$setIntersection: ["$skills", loggedInUserSkills]}
+                        }
+                    }
+                },
+                {
+                    $sort: {
+                    isCloseInAge: -1,         // First priority: age range match
+                    commonSkillsCount: -1,  // Second: more common skills
+                    age: 1               // Optional: sort younger users first
+                    }
+                },
+                {
+                    $project: {
+                    password: 0,
+                    emailId: 0,
+                    __v: 0,
+                    createdAt: 0,
+                    updatedAt: 0
+                    }
+                }
+        ]);
+
+
+
+        res.json({data : users});
+        // res.json({data : [...priorityUsersOnfeed,...normalUsersOnfeed]});
+
     }catch(err){
         res.status(500).send(err.message);
     }
