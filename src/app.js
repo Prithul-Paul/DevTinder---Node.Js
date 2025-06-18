f492630dfc9c20f785f6724abdf558b478a15cf7
prithul-paul <ptithulpaul@gmail.com>
2025-06-19 02:39:37 +0530

    Changes - 19/06/2025

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 const express = require("express");
-const cookiePerser = require("cookie-parser");
 const cors = require("cors");
 const mongoDBConnection = require("./config/database");
+const cookieParser = require("cookie-parser");
 
 const authRouters = require("./routes/auth");
 const profileRouters = require("./routes/profile");
 const requestRouters = require("./routes/request");
 const userRouters = require("./routes/user");
-
-const cookieParser = require("cookie-parser");
-var cors = require('cors');
 const app = express();
 
 app.use(cors({
